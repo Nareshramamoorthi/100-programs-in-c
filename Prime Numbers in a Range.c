4b49f113c64ed9548ae3de58d60252f5a2e326c1
Naresh R <140954938+Nareshramamoorthi@users.noreply.github.com>
2024-10-17 18:15:13 +0530

    Add files via upload

---
+++ Prime Numbers in a Range.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+void findPrimesInRange(int start, int end) {
+    printf("Prime numbers between %d and %d are:\n", start, end);
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int start, end;
+    printf("Enter the start of the range: ");
+    scanf("%d", &start);
+    printf("Enter the end of the range: ");
+    scanf("%d", &end);
+
+    if (start > end) {
+        printf("Invalid range! Start should be less than or equal to end.\n");
+        return 1;
+    }
+
+    findPrimesInRange(start, end);
+
+    return 0;
+}
+s
