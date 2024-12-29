32916f55cf9af8581d2b5b9161ebb33e2a139280
Naresh R <140954938+Nareshramamoorthi@users.noreply.github.com>
2024-12-29 08:12:52 +0530

    Add files via upload

---
+++ Find the Middle Element of a Linked List.c
@@ -0,0 +1,66 @@
+/*
+Title: Find the Middle Element of a Linked List
+Description: This program finds the middle element of a linked list using a two-pointer approach.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a linked list node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to find the middle element of the linked list
+void findMiddle(struct Node* head) {
+    struct Node* slow = head;
+    struct Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if (slow != NULL) {
+        printf("The middle element is: %d\n", slow->data);
+    } else {
+        printf("The linked list is empty.\n");
+    }
+}
+
+// Function to print the linked list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    // Creating a linked list
+    head = createNode(10);
+    head->next = createNode(20);
+    head->next->next = createNode(30);
+    head->next->next->next = createNode(40);
+    head->next->next->next->next = createNode(50);
+
+    printf("Linked List: ");
+    printList(head);
+
+    // Find and print the middle element
+    findMiddle(head);
+
+    return 0;
+}
