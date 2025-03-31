efa5e0ecb2003c9c64df8c850f5307755882f2f2
Naresh R <140954938+Nareshramamoorthi@users.noreply.github.com>
2025-03-31 17:18:43 +0530

    Add files via upload

--- Matrix Spiral Traversal.c
+++ Matrix Spiral Traversal.c
@@ -1,62 +1,43 @@
 #include <stdio.h>
 
-#define N 4 // Number of rows and columns
+#define SIZE 4
 
-// Function to print a matrix in spiral order
-void printSpiral(int matrix[N][N]) {
-    int top = 0, bottom = N - 1, left = 0, right = N - 1;
-
-    printf("Spiral Traversal of the Matrix:\n");
+void spiralOrder(int matrix[SIZE][SIZE], int n) {
+    int top = 0, bottom = n - 1, left = 0, right = n - 1;
 
     while (top <= bottom && left <= right) {
-        // Traverse from left to right
-        for (int i = left; i <= right; i++) {
+        for (int i = left; i <= right; i++)
             printf("%d ", matrix[top][i]);
-        }
         top++;
 
-        // Traverse from top to bottom
-        for (int i = top; i <= bottom; i++) {
+        for (int i = top; i <= bottom; i++)
             printf("%d ", matrix[i][right]);
-        }
         right--;
 
-        // Traverse from right to left (if rows remain)
         if (top <= bottom) {
-            for (int i = right; i >= left; i--) {
+            for (int i = right; i >= left; i--)
                 printf("%d ", matrix[bottom][i]);
-            }
             bottom--;
         }
 
-        // Traverse from bottom to top (if columns remain)
         if (left <= right) {
-            for (int i = bottom; i >= top; i--) {
+            for (int i = bottom; i >= top; i--)
                 printf("%d ", matrix[i][left]);
-            }
             left++;
         }
     }
-    printf("\n");
 }
 
 int main() {
-    int matrix[N][N] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
+    int matrix[SIZE][SIZE] = {
+        {1,  2,  3,  4},
+        {5,  6,  7,  8},
+        {9,  10, 11, 12},
         {13, 14, 15, 16}
     };
 
-    printf("Original Matrix:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    printSpiral(matrix);
+    printf("Spiral Order Traversal:\n");
+    spiralOrder(matrix, SIZE);
 
     return 0;
 }
