468a26c031c2a0d5ea6cc72f515d8e7da6d86ee7
Naresh R <140954938+Nareshramamoorthi@users.noreply.github.com>
2025-04-01 21:02:21 +0530

    Add files via upload

--- Magic Square Generator.c
+++ Magic Square Generator.c
@@ -1,44 +1,26 @@
 #include <stdio.h>
 
+#define SIZE 3 
+
 void generateMagicSquare(int n) {
-    int magicSquare[15][15];
+    int magicSquare[SIZE][SIZE] = {0};
 
-    // Initialize the magic square with 0
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            magicSquare[i][j] = 0;
-
-    // Set initial position
-    int num = 1;
-    int row = 0;
-    int col = n / 2;
-    int current_row, current_col;
-
-    while (num <= n * n) {
-        magicSquare[row][col] = num;
-        num++;
-        current_row = row;
-        current_col = col;
-
-        // Move up and right
-        row -= 1;
-        col += 1;
-
-        // Wrap around if out of bounds
-        if (row < 0) row = n - 1;
-        if (col == n) col = 0;
-
-        // If the cell is already filled, move down instead
-        if (magicSquare[row][col] != 0) {
-            row = current_row + 1;
-            col = current_col;
+    int i = 0, j = n / 2;
+    for (int num = 1; num <= n * n; num++) {
+        magicSquare[i][j] = num;
+        int newI = (i - 1 + n) % n;
+        int newJ = (j + 1) % n;
+        if (magicSquare[newI][newJ] != 0) {
+            i = (i + 1) % n;
+        } else {
+            i = newI;
+            j = newJ;
         }
     }
 
-    // Display the magic square
     printf("Magic Square of size %d:\n", n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             printf("%3d ", magicSquare[i][j]);
         }
         printf("\n");
@@ -46,17 +28,6 @@ void generateMagicSquare(int n) {
 }
 
 int main() {
-    int n;
-    printf("Enter the size of the magic square (odd number): ");
-    scanf("%d", &n);
-
-    // Ensure the input is an odd number
-    if (n % 2 == 0) {
-        printf("Please enter an odd number.\n");
-        return 1;
-    }
-
-    generateMagicSquare(n);
-
+    generateMagicSquare(SIZE);
     return 0;
 }
