2dfcb22f8012678beaa8d2080833f9f6b91f1712
Naresh R <140954938+Nareshramamoorthi@users.noreply.github.com>
2025-04-14 05:56:52 +0530

    Add files via upload

---
+++ Matrix Border Sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int mat[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int sum = 0;
+    int n = 3;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || i == n-1 || j == 0 || j == n-1)
+                sum += mat[i][j];
+        }
+    }
+
+    printf("Border sum = %d\n", sum);
+    return 0;
+}
